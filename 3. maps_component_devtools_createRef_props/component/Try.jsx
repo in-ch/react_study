01ce0336e3bde2a88c4b1da546a2c46fc0d1ed29
in-ch <sxin2949@naver.com>
2020-10-09 00:12:43 +0900

    3-13  props와 state 연결하기

--- 3. maps_component_devtools_createRef_props/component/Try.jsx
+++ 3. maps_component_devtools_createRef_props/component/Try.jsx
@@ -1,12 +1,18 @@
 const React = require('react');
+const { useState, useRef, Component, memo } = React;
 
 const Try = ({tryInfo}) => {
+    
+    const [result, setResult] = useState(tryInfo.result);
+    const onClick = () => {
+        setResult('1');
+    }   // 3-13에 적혀 있음. props는 직접 바꾸면 안됨.
+    
     return (
         <>
             <li>
                 <div>{tryInfo.try}</div>
-                <div>{tryInfo.result}</div>
-                
+                <div onClick = {onClick}>{result}</div>
             </li>
         </>
     )

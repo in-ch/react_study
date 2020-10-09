ce9f2c965459d073209cb55c74b9d07546b70896
in-ch <sxin2949@naver.com>
2020-10-09 21:07:55 +0900

    4-1  React 조건문

--- 4. reaction_velocity/component/ResponseCheck.jsx
+++ 4. reaction_velocity/component/ResponseCheck.jsx
@@ -2,11 +2,26 @@ const React = require('react');
 const { useState, useRef, Component } = React;
 
 const ResponseCheck = () => {
+    const [state, setState] = useState('waiting');
+    const [message, setMessage] = useState('클릭해서 시작하세요.');
+    const [result, setResult] = useState([]);
+
+    const onClickScreen = () => {
+    };
+
     return (
         <>
-            <div>
-                <h5>Hello 4lecture</h5>
+            <div id="screen" className={state} onClick={onClickScreen}>
+                <h5>{message}</h5>
             </div>
+
+            {
+                result.length === 0 ? null : 
+                <div>
+                    평균 시간 : {result.reduce((a,c)=> a + c ) / result.length } ms   
+                </div>
+            }
+            
         </>
     );
 }

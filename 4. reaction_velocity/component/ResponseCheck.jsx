704048deb92658882a977e0e3ee62f21ccd5c9a6
in-ch <sxin2949@naver.com>
2020-10-09 21:25:55 +0900

    4-2 setTimeout 넣어 반응속도 체크

--- 4. reaction_velocity/component/ResponseCheck.jsx
+++ 4. reaction_velocity/component/ResponseCheck.jsx
@@ -1,12 +1,36 @@
 const React = require('react');
 const { useState, useRef, Component } = React;
 
+let timeout; 
+let startTime;
+let endTime;
 const ResponseCheck = () => {
     const [state, setState] = useState('waiting');
     const [message, setMessage] = useState('클릭해서 시작하세요.');
     const [result, setResult] = useState([]);
 
     const onClickScreen = () => {
+        if(state === 'waiting') {
+            setState('ready');
+            setMessage('초록색이 되면 클릭해주세요.');
+            timeout = setTimeout(() => {
+                setState('now');
+                setMessage('지금 클릭하세요.');
+                startTime = new Date(); 
+            }, Math.floor(Math.random()* 1000) + 2000 );
+        } else if(state === 'ready') {
+            setState('waiting');
+            setMessage('이런 성급하셨군요.');
+            clearTimeout(timeout);
+        } else if(state === 'now') {
+            endTime = new Date(); 
+
+            setState('waiting');
+            setMessage('클릭해서 시작하세요.');
+            setResult((prevResult) => {
+                return [...prevResult, endTime - startTime];
+            });
+        }
     };
 
     return (

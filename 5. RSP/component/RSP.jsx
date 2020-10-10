afb6b161b5fee5f9e1331a7682ecbb12c31beda7
in-ch <sxin2949@naver.com>
2020-10-10 21:31:06 +0900

    setting 5 lecture

---
+++ 5. RSP/component/RSP.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+const { useState, useRef, Component } = React;
+
+const RSP = () => {
+  
+    
+    return (
+        <>
+            <h5>Hello world</h5>
+        </>
+    );
+}
+
+
+module.exports = RSP;

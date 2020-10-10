b7a085552406924d78b117cd05700a4f3570ed64
in-ch <sxin2949@naver.com>
2020-10-10 21:11:38 +0900

    4-4 Hooks로 전환하기

--- 4. reaction_velocity/component/ResponseCheck.jsx
+++ 4. reaction_velocity/component/ResponseCheck.jsx
@@ -1,38 +1,48 @@
 const React = require('react');
 const { useState, useRef, Component } = React;
-
+/*
 let timeout; 
 let startTime;
 let endTime;
+*/
 const ResponseCheck = () => {
     const [state, setState] = useState('waiting');
     const [message, setMessage] = useState('클릭해서 시작하세요.');
     const [result, setResult] = useState([]);
-
+    const timeout = useRef(null);
+    const startTime = useRef();
+    const endTime = useRef();
+    
     const onClickScreen = () => {
         if(state === 'waiting') {
             setState('ready');
             setMessage('초록색이 되면 클릭해주세요.');
-            timeout = setTimeout(() => {
+            timeout.current = setTimeout(() => {
                 setState('now');
                 setMessage('지금 클릭하세요.');
-                startTime = new Date(); 
+                startTime.current = new Date(); 
             }, Math.floor(Math.random()* 1000) + 2000 );
         } else if(state === 'ready') {
             setState('waiting');
             setMessage('이런 성급하셨군요.');
-            clearTimeout(timeout);
+            clearTimeout(timeout.current);
         } else if(state === 'now') {
-            endTime = new Date(); 
+            endTime.current = new Date(); 
 
             setState('waiting');
             setMessage('클릭해서 시작하세요.');
             setResult((prevResult) => {
-                return [...prevResult, endTime - startTime];
+                return [...prevResult, endTime.current - startTime.current];
             });
         }
     };
+    const renderAverage = () => {
+    }
 
+    const onReset = () => {
+        setResult([]);
+    }
+    
     return (
         <>
             <div id="screen" className={state} onClick={onClickScreen}>
@@ -45,7 +55,7 @@ const ResponseCheck = () => {
                     평균 시간 : {result.reduce((a,c)=> a + c ) / result.length } ms   
                 </div>
             }
-            
+            <button onClick={onReset}>리셋</button>
         </>
     );
 }

c6b17a636a41ea3902ffdc5f0612ee3d2a60aeed
in-ch <sxin2949@naver.com>
2020-10-13 22:39:09 +0900

    setting 6 lecture

---
+++ 6. Lotto/component/Lotto.jsx
@@ -0,0 +1,19 @@
+const { useEffect } = require('react');
+const React = require('react');
+const { useState, useRef, Component } = React;
+
+
+
+const Lotto = () => {
+  
+    return (
+        <>
+            <div>
+                <h5>Hello, Rotto Component</h5>
+                
+            </div>
+        </>
+    )
+}
+
+module.exports = Lotto;

f9bfde8ac61ae6f45713a5b5421ec0f54929d0de
in-ch <sxin2949@naver.com>
2020-10-19 22:42:55 +0900

    setting 7lecture

---
+++ 7. TikTakTok/component/tiktaktok.jsx
@@ -0,0 +1,18 @@
+//const { useEffect } = require('react');
+const React = require('react');
+//const memo = require('react');
+const { useState, useRef, Component } = React;
+
+const tiktaktok = () => {
+    
+
+    return (
+        <>
+            <div >
+                <h5>hello, world</h5>
+            </div>
+        </>
+    );
+}
+
+module.exports = tiktaktok;

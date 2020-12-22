c9e43201b5feb74ccccfd59208abac8d0eb121a4
inch <sxin2949@naver.com>
2020-12-22 16:37:56 +0900

    webpack low version download

---
+++ 7.2TikTakTok/component/TikTakTok.jsx
@@ -0,0 +1,16 @@
+const React = require('react');
+const { useState, useRef, Component } = React;
+
+const TikTakTok = () => {
+
+    return (
+            <>
+                <div>
+                    <h3>Hello, world
+                    </h3>
+                </div>
+            </>
+        )
+}
+
+module.exports = TikTakTok;
